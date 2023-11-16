11e047e17bc13d16a43eea508a210f6fe9e85fe6
marianepsantos <marianepsantos15@gmail.com>
2023-11-16 16:35:58 -0400

    Commit pronto pronto

--- app.js
+++ app.js
@@ -7,10 +7,26 @@ app.use(express.static('public'));
 
 const produtos = [
   {id: 1, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
+  {id: 2, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
+  {id: 3, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
+  {id: 4, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
+  {id: 5, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
+  {id: 6, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
+  {id: 7, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
+  {id: 8, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
+  {id: 9, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
+  {id: 10, nome: "Body Splash Infinity", preço: "R$99,00", descricao: "Transmita determinação da melhor forma com WEPINK", imagem: "https://wepink.vtexassets.com/arquivos/ids/156226-800-auto?v=638017234609830000&width=800&height=auto&aspect=true" },
 ]
 
+
+function buscarProdutoPorID(){
+  const produto = produtos.find(produto => produto.id == 3);
+  return produto || null
+}
+
+
 app.get('/produto/:id', (req, res) => {
-  res.render('produto', { message: 'Bem vindo a página de produtos!' });
+  res.render('produto', {produtos });
 });
 
 app.get('/', (req, res) => {
